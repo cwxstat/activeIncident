c8d5d3a3c87b7c323d1b07972dfd5bd889a21395
Mike Chirico <mchirico@gmail.com>
2022-04-28 17:51:34 +0000

    update-automated-save

---
+++ cmd/probe.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// probeCmd represents the probe command
+var probeCmd = &cobra.Command{
+	Use:   "probe",
+	Short: "liveliness probe",
+	Long: `Liveliness probe is a tool to check if the application is alive.
+
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("probe called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(probeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// probeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// probeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
