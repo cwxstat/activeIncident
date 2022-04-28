2bce06b001e4f5b4246f2fb1808e8908e771ba11
Mike Chirico <mchirico@gmail.com>
2022-04-28 13:52:31 -0400

    too many connections... connect to mongodb in wrong place... attempt to fix
    * update-automated-save
    
    * update-automated-save
    
    * update-automated-save
    
    * update-automated-save

--- cmd/root.go
+++ cmd/root.go
@@ -29,21 +29,18 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithTimeout(context.TODO(), time.Second*30)
+		defer cancel()
+		as, err := db.NewActiveIncidentServer(ctx)
+		if err != nil {
+			log.Panic(err)
+		}
 
 		for {
 
 			a, err := scrape.AddDB()
 			if err != nil {
 				log.Println(err)
-			}
-
-			ctx, cancel := context.WithTimeout(context.TODO(), time.Second*30)
-			defer cancel()
-			as, err := db.NewActiveIncidentServer(ctx)
-			if err != nil {
-				log.Println(err)
-				time.Sleep(constants.ErrorBackoff)
-				continue
 			}
 
 			err = as.AddEntry(ctx, a)
