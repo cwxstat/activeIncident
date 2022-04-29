fedc6504de321554957c510b9179c3a9c2506dd1
Mike Chirico <mchirico@gmail.com>
2022-04-29 19:03:15 +0000

    update-automated-save

--- cmd/populate_test.go
+++ cmd/populate_test.go
@@ -6,12 +6,12 @@ import (
 	"time"
 
 	"github.com/cwxstat/activeIncident/db"
-	"github.com/cwxstat/activeIncident/dbinterface"
+	"github.com/cwxstat/activeIncident/dbpop"
 )
 
 func TestPopulate(t *testing.T) {
 
-	a, err := dbinterface.AddDB()
+	a, err := dbpop.AddDB()
 	if err != nil {
 		t.Fatal(err)
 	}
