77e2f5dae68bd1aa8bb18453a6afc5c484f407b9
Mike Chirico <mchirico@gmail.com>
2023-01-14 18:41:43 -0500

    populate incident fix and added Test_populateIncident (#41)

--- dbpop/dbpop_test.go
+++ dbpop/dbpop_test.go
@@ -1,6 +1,10 @@
 package dbpop
 
 import (
+	"bytes"
+	"encoding/gob"
+	"github.com/cwxstat/activeIncident/fixtures"
+	"reflect"
 	"testing"
 
 	"github.com/cwxstat/activeIncident/active"
@@ -28,4 +32,48 @@ func TestAddDB(t *testing.T) {
 			_ = got
 		})
 	}
+}
+
+func Test_populateIncident(t *testing.T) {
+
+	var network bytes.Buffer // Stand-in for a network connection
+	dec := gob.NewDecoder(&network)
+	path := fixtures.Path("./testdata/scrapeMainTable.enc")
+	buf, err := fixtures.Read(path)
+	n, err := network.Write(buf)
+	if err != nil || n == 0 {
+		t.Fatal(err)
+	}
+
+	var list []string
+	err = dec.Decode(&list)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := populateIncident(list)
+	if err != nil {
+		t.Fatal(err)
+	}
+	e, err := ExpectedValue()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(result, e) {
+		t.Fail()
+	}
+}
+
+func ExpectedValue() ([]active.Incident, error) {
+	var network bytes.Buffer // Stand-in for a network connection
+	a := []active.Incident{}
+	dec := gob.NewDecoder(&network)
+	path := fixtures.Path("./testdata/expectedResult.enc")
+	buf, err := fixtures.Read(path)
+	n, err := network.Write(buf)
+	if err != nil || n == 0 {
+		return a, err
+	}
+	err = dec.Decode(&a)
+	return a, err
 }
